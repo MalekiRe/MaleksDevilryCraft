c97ae6f4bc640ebfb7e744bbdd21745a2a6d5b8a
BuildTools <unconfigured@null.spigotmc.org>
2021-04-27 17:08:32 -0700

    Added Ore Generation for Earth, Air, Fire, Water, and Tainted Crystals.

--- src/main/java/malekire/devilrycraft/common/DevilryOreGeneration.java
+++ src/main/java/malekire/devilrycraft/common/DevilryOreGeneration.java
@@ -1,7 +1,7 @@
 package malekire.devilrycraft.common;
 
 import malekire.devilrycraft.Devilrycraft;
-import malekire.devilrycraft.generation.crystal_generation.VisCrystalGenerationFeature;
+import malekire.devilrycraft.generation.crystal_generation.*;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.block.Blocks;
@@ -27,14 +27,27 @@ public class DevilryOreGeneration {
 
 
     public static final Feature<OreFeatureConfig> VIS_CRYSTAL = new VisCrystalGenerationFeature(OreFeatureConfig.CODEC);
+    public static final Feature<OreFeatureConfig> TAINT_CRYSTAL = new TaintCrystalGenerationFeature(OreFeatureConfig.CODEC);
+    public static final Feature<OreFeatureConfig> EARTH_CRYSTAL = new EarthCrystalGenerationFeature(OreFeatureConfig.CODEC);
+    public static final Feature<OreFeatureConfig> AIR_CRYSTAL = new AirCrystalGenerationFeature(OreFeatureConfig.CODEC);
+    public static final Feature<OreFeatureConfig> FIRE_CRYSTAL = new FireCrystalGenerationFeature(OreFeatureConfig.CODEC);
+    public static final Feature<OreFeatureConfig> WATER_CRYSTAL = new WaterCrystalGenerationFeature(OreFeatureConfig.CODEC);
 
 
 
-
-    public static final ConfiguredFeature<?, ?> VIS_CRYSTAL_CONFIGURED = VIS_CRYSTAL.configure(ORE_FEATURE_CONFIG).decorate(Decorator.RANGE.configure(RANGE_DECORATOR_CONFIG).spreadHorizontally().repeat(50));
-
+    public static final ConfiguredFeature<?, ?> VIS_CRYSTAL_CONFIGURED = VIS_CRYSTAL.configure(ORE_FEATURE_CONFIG).decorate(Decorator.RANGE.configure(RANGE_DECORATOR_CONFIG).repeat(20));
+    public static final ConfiguredFeature<?, ?> TAINT_CRYSTAL_CONFIGURED = TAINT_CRYSTAL.configure(ORE_FEATURE_CONFIG).decorate(Decorator.RANGE.configure(RANGE_DECORATOR_CONFIG).repeat(20));
+    public static final ConfiguredFeature<?, ?> EARTH_CRYSTAL_CONFIGURED = EARTH_CRYSTAL.configure(ORE_FEATURE_CONFIG).decorate(Decorator.RANGE.configure(RANGE_DECORATOR_CONFIG).repeat(20));
+    public static final ConfiguredFeature<?, ?> AIR_CRYSTAL_CONFIGURED = AIR_CRYSTAL.configure(ORE_FEATURE_CONFIG).decorate(Decorator.RANGE.configure(RANGE_DECORATOR_CONFIG).repeat(20));
+    public static final ConfiguredFeature<?, ?> FIRE_CRYSTAL_CONFIGURED = FIRE_CRYSTAL.configure(ORE_FEATURE_CONFIG).decorate(Decorator.RANGE.configure(RANGE_DECORATOR_CONFIG).repeat(20));
+    public static final ConfiguredFeature<?, ?> WATER_CRYSTAL_CONFIGURED = WATER_CRYSTAL.configure(ORE_FEATURE_CONFIG).decorate(Decorator.RANGE.configure(RANGE_DECORATOR_CONFIG).repeat(20));
     static {
-        features.add(new FeatureGroup(VIS_CRYSTAL_CONFIGURED, VIS_CRYSTAL, new Identifier(Devilrycraft.MOD_ID, "crystal")));
+        features.add(new FeatureGroup(VIS_CRYSTAL_CONFIGURED, VIS_CRYSTAL, new Identifier(Devilrycraft.MOD_ID, "vis_crystal")));
+        features.add(new FeatureGroup(TAINT_CRYSTAL_CONFIGURED, TAINT_CRYSTAL, new Identifier(Devilrycraft.MOD_ID, "taint_crystal")));
+        features.add(new FeatureGroup(EARTH_CRYSTAL_CONFIGURED, EARTH_CRYSTAL, new Identifier(Devilrycraft.MOD_ID, "earth_crystal")));
+        features.add(new FeatureGroup(AIR_CRYSTAL_CONFIGURED, AIR_CRYSTAL, new Identifier(Devilrycraft.MOD_ID, "air_crystal")));
+        features.add(new FeatureGroup(FIRE_CRYSTAL_CONFIGURED, FIRE_CRYSTAL, new Identifier(Devilrycraft.MOD_ID, "fire_crystal")));
+        features.add(new FeatureGroup(WATER_CRYSTAL_CONFIGURED, WATER_CRYSTAL, new Identifier(Devilrycraft.MOD_ID, "water_crystal")));
 
     }
 
---
+++ src/main/java/malekire/devilrycraft/generation/crystal_generation/TaintCrystalGenerationFeature.java
@@ -0,0 +1,20 @@
+package malekire.devilrycraft.generation.crystal_generation;
+
+import com.mojang.serialization.Codec;
+import malekire.devilrycraft.common.DevilryBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+
+import java.util.Random;
+
+public class TaintCrystalGenerationFeature extends CrystalGenerationFeature{
+    public TaintCrystalGenerationFeature(Codec<OreFeatureConfig> config) {
+        super(config);
+    }
+    public BlockState getBlockState(Random random, BlockPos pos, StructureWorldAccess world) {
+        return DevilryBlocks.TAINT_CRYSTAL_BLOCK.getBlockStateFromPos(pos, world).with(Properties.PICKLES, random.nextInt(3)+1);
+    }
+}
