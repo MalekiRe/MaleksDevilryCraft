ad83954033eaf278092a8c496418e05cb130a62a
MalekiRe <pocmalek@gmail.com>
2021-05-10 14:30:22 -0700

    Fixed fire seal so it doesn't set items on fire

--- src/main/java/malekire/devilrycraft/objects/blockentities/sealhelpers/SealFireHelper.java
+++ src/main/java/malekire/devilrycraft/objects/blockentities/sealhelpers/SealFireHelper.java
@@ -3,6 +3,7 @@ package malekire.devilrycraft.objects.blockentities.sealhelpers;
 import malekire.devilrycraft.util.CrystalType;
 import malekire.devilrycraft.util.CrystalType;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Box;
 
@@ -25,7 +26,7 @@ public class SealFireHelper extends AbstractSealHelperClass{
         Box flame = new Box(posX-2, posY-2, posZ-2, posX+2, posY+2, posZ+2);
 
         assert this.world != null;
-        List<Entity> entitiesToFlame = this.world.getOtherEntities(null, flame, (entity) -> !(entity instanceof PlayerEntity && entity.isPushable()));
+        List<Entity> entitiesToFlame = this.world.getOtherEntities(null, flame, (entity) -> !(entity instanceof PlayerEntity) && entity.isPushable() && !(entity instanceof ItemEntity));
         for(i=0;i<entitiesToFlame.size();i++){
             entitiesToFlame.get(i).setOnFireFor(100);
         }
