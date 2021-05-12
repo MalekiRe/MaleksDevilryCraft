c951b900b9be8836b8e82afbc465cfd3017cf6a2
gamma-02 <dposselt24@pilgrim-school.org>
2021-05-12 11:43:15 -0700

    textures are finished, for strider boots

--- src/main/java/malekire/devilrycraft/objects/blockentities/StrideBlocks/LavaStrideBlockEntity.java
+++ src/main/java/malekire/devilrycraft/objects/blockentities/StrideBlocks/LavaStrideBlockEntity.java
@@ -3,19 +3,25 @@ package malekire.devilrycraft.objects.blockentities.StrideBlocks;
 import malekire.devilrycraft.common.DevilryBlockEntities;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.scoreboard.ScoreboardCriterion;
+import net.minecraft.util.Tickable;
 
-public class LavaStrideBlockEntity extends BlockEntity {
+public class LavaStrideBlockEntity extends BlockEntity implements Tickable {
 
     public LavaStrideBlockEntity() {
         super(DevilryBlockEntities.LAVA_STRIDE_BLOCK_ENTITY);
     }
 
-    private int strideTick;
+
+
+    int lavaTicksSinceCreation = 0;
+
     public void tick() {
-        if(strideTick>20){
+        if(lavaTicksSinceCreation>20){
             this.world.setBlockState(getPos(), Blocks.LAVA.getDefaultState(), 2);
         }
-        strideTick++;
+        lavaTicksSinceCreation++;
     }
 
 }
--- src/main/java/malekire/devilrycraft/objects/blockentities/StrideBlocks/WaterStrideBlockEntity.java
+++ src/main/java/malekire/devilrycraft/objects/blockentities/StrideBlocks/WaterStrideBlockEntity.java
@@ -4,20 +4,24 @@ import malekire.devilrycraft.common.DevilryBlockEntities;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.scoreboard.ScoreboardCriterion;
+import net.minecraft.util.Tickable;
 
-public class WaterStrideBlockEntity extends BlockEntity {
+public class WaterStrideBlockEntity extends BlockEntity implements Tickable {
 
     public WaterStrideBlockEntity() {
         super(DevilryBlockEntities.WATER_STRIDE_BLOCK_ENTITY);
     }
 
-    private int strideTick;
+
+
+    int waterTicksSinceCreation = 0;
 
     public void tick() {
-        if(strideTick>20){
+        if(waterTicksSinceCreation>20){
             this.world.setBlockState(getPos(), Blocks.WATER.getDefaultState(), 2);
         }
-        strideTick++;
+        waterTicksSinceCreation++;
     }
 
 }
