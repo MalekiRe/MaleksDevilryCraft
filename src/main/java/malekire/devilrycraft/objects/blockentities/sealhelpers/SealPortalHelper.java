06b851679432f9f949cf2ae81e516867c97048e1
MalekiRe <pocmalek@gmail.com>
2021-05-12 19:28:16 -0700

    Did some refactoring and clean up on the SealPortalHelper

--- src/main/java/malekire/devilrycraft/objects/blockentities/sealhelpers/SealPortalHelper.java
+++ src/main/java/malekire/devilrycraft/objects/blockentities/sealhelpers/SealPortalHelper.java
@@ -4,15 +4,18 @@ import com.qouteall.immersive_portals.my_util.DQuaternion;
 import com.qouteall.immersive_portals.portal.Portal;
 import com.qouteall.immersive_portals.portal.PortalManipulation;
 import malekire.devilrycraft.Devilrycraft;
+import malekire.devilrycraft.common.DevilrySounds;
 import malekire.devilrycraft.util.CrystalType;
 import malekire.devilrycraft.util.math.beziercurves.BezierCurve;
 import malekire.devilrycraft.util.math.beziercurves.Point;
 import malekire.devilrycraft.util.portalutil.PortalFinderUtil;
 import malekire.devilrycraft.util.portalutil.PortalFunctionUtil;
 import net.minecraft.block.Blocks;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.apache.logging.log4j.Level;
 import org.lwjgl.system.CallbackI;
 
@@ -26,6 +29,8 @@ public class SealPortalHelper extends AbstractSealHelper {
     double maxHeight = 3;
     public double width = 0;
     public double height = 0;
+    public static final double INITAL_VISUAL_WIDTH = 0.02;
+    public static final double INITAL_VISUAL_HEIGHT = 0.02;
     double timeValue = 0;
     public double PORTAL_OFFSET_ANIMATION_TICKS = 0.0;
     public double PORTAL_ANIMATION_TICKS = 80.0;
@@ -38,8 +43,9 @@ public class SealPortalHelper extends AbstractSealHelper {
     public static final int LIGHTNING_SPAWN_CHANCE = 10;
     int ticksSinceCreation = 0;
     public boolean doSecondSound = false;
-    public SealBlockEntity opposingSealBlockEntity;
-
+    boolean growPortal = false;
+    public int tickTime = 0;
+    boolean shrinkIsAnimated = false;
     public SealPortalHelper() {
         super(PortalSealID, PortalSealID.sealCombinations);
         this.isMateable = true;
@@ -53,11 +59,11 @@ public class SealPortalHelper extends AbstractSealHelper {
         bezierCurve.addPoint(new Point(1, 1));
     }
 
-    public int tickTime = 0;
+
 
     @Override
     public void tick() {
-        /*
+
         if (tickTime > 3) {
             tickTime = 3;
         }
@@ -78,10 +84,9 @@ public class SealPortalHelper extends AbstractSealHelper {
             }
         }
 
-         */
+
     }
 
-    boolean shrinkIsAnimated = false;
 
     public void animatePortals(double timeValue) {
         width = bezierCurve.getY(timeValue) * ((double) maxWidth);
@@ -125,88 +130,79 @@ public class SealPortalHelper extends AbstractSealHelper {
     double portalDestructionAnimationTicks = PORTAL_ANIMATION_TICKS;
 
     public void performPortalFunction() {
-        if(this.mate == null) {
-            Devilrycraft.LOGGER.log(Level.INFO, "Performed portal function but no matable seal found");
+        if (this.mate == null) {
+            Devilrycraft.LOGGER.log(Level.DEBUG, "Performed portal function but no mateable seal found");
             return;
         }
         if (world.getBlockState(pos).getBlock() == Blocks.AIR)
             return;
-        System.out.println("performing portal fucntion 2");
-        CrystalType firstCode = world.getBlockState(pos).get(THIRD_LAYER);
-        CrystalType secondCode = world.getBlockState(pos).get(FOURTH_LAYER);
         BlockPos portalPosition = this.blockEntity.getPos();
         BlockPos outputPos;
-        System.out.println("has portal : " + hasPortal);
-        if (hasPortal)
-            return;
-        System.out.println("preforming oprtal function 3");
+        Devilrycraft.LOGGER.log(Level.INFO, "creating portal in seal gateway");
+        hasPortal = true;
+        outputPos = mate.blockEntity.getPos();
+        this.entrancePortal = Portal.entityType.create(world);
+        final float portalVisualOffset = 0.48F;
+
+        //Sets position to center of block.
+        Vec3d originPos = Vec3d.ofCenter(portalPosition);
+
+        //Sets position to center of block.
+        Vec3d destPos = Vec3d.ofCenter(outputPos);
 
 
+        entrancePortal.setDestinationDimension(mate.blockEntity.getWorld().getRegistryKey());
 
-            System.out.println("making portal");
-            hasPortal = true;
-            outputPos = mate.blockEntity.getPos();
-            this.entrancePortal = Portal.entityType.create(world);
-            final float portalVisualOffset = 0.48F;
+        originPos = PortalFunctionUtil.offsetFromFacing(originPos, blockEntity.facing, portalVisualOffset);
+        destPos = PortalFunctionUtil.offsetFromFacing(destPos, mate.blockEntity.facing, portalVisualOffset);
 
-            //Sets position to center of block.
-            Vec3d originPos = Vec3d.ofCenter(portalPosition);
+        entrancePortal.setOriginPos(originPos);
+        entrancePortal.setDestination(destPos);
+        double rotation = PortalFunctionUtil.getDegreeFromDirectionForPortal(blockEntity.facing);
+        double degrees = 180 + PortalFunctionUtil.getDegreeFromDirectionForPortal(mate.blockEntity.facing) - rotation;
 
-            //Sets position to center of block.
-            Vec3d destPos = Vec3d.ofCenter(outputPos);
+        entrancePortal.setRotationTransformation(DQuaternion.rotationByDegrees(new Vec3d(0, 1, 0), degrees).toMcQuaternion());
+        setPortalOrientationAndSizeFromDirection(blockEntity.facing, entrancePortal, rotation);
 
-
-            Direction reverseFacing = blockEntity.facing.getOpposite();
-
-            entrancePortal.setDestinationDimension(mate.blockEntity.getWorld().getRegistryKey());
+        entrancePortal.world.spawnEntity(entrancePortal);
 
 
-            originPos = PortalFunctionUtil.offsetFromFacing(originPos, blockEntity.facing, portalVisualOffset);
-            destPos = PortalFunctionUtil.offsetFromFacing(destPos, mate.blockEntity.facing, portalVisualOffset);
-
-            entrancePortal.setOriginPos(originPos);
-            entrancePortal.setDestination(destPos);
-            double rotation = PortalFunctionUtil.getDegreeFromDirectionForPortal(blockEntity.facing);
-            double degrees = 180 + PortalFunctionUtil.getDegreeFromDirectionForPortal(mate.blockEntity.facing) - rotation;
-
-            entrancePortal.setRotationTransformation(DQuaternion.rotationByDegrees(new Vec3d(0, 1, 0), degrees).toMcQuaternion());
-            float rotation2 = 90;
-
-            switch (reverseFacing) {
-                case UP:
-                    entrancePortal.setOrientationAndSize(
-                            new Vec3d(1, 0, 0).rotateY((float) Math.toRadians(rotation)), // axisW
-                            new Vec3d(0, 1, 0).rotateX((float) Math.toRadians(-rotation2)), // axisH
-                            1, // width
-                            2 // height
-                    );
-                    break;
-                case DOWN:
-                    entrancePortal.setOrientationAndSize(
-                            new Vec3d(1, 0, 0).rotateY((float) Math.toRadians(rotation)), // axisW
-                            new Vec3d(0, 1, 0).rotateX((float) Math.toRadians(rotation2)), // axisH
-                            1, // width
-                            2 // height
-                    );
-                    break;
-                default:
-                    entrancePortal.setOrientationAndSize(
-                            new Vec3d(1, 0, 0).rotateY((float) Math.toRadians(rotation)), // axisW
-                            new Vec3d(0, 1, 0), // axisH
-                            1, // width
-                            2 // height
-                    );
-                    break;
-            }
-            entrancePortal.world.spawnEntity(entrancePortal);
+        exitPortal = PortalManipulation.completeBiWayPortal(entrancePortal, Portal.entityType);
 
 
-            //exitPortal = PortalManipulation.completeBiWayPortal(entrancePortal, Portal.entityType);
+        return;
 
 
-            return;
-
-
+    }
+    public static void setPortalOrientationAndSizeFromDirection(Direction facing, Portal portal, double rotation1) {
+        //This is just the value that makes this work don't question it please lol.
+        float rotation2 = 90;
+        switch (facing) {
+            case UP:
+                portal.setOrientationAndSize(
+                        new Vec3d(1, 0, 0).rotateY((float) Math.toRadians(rotation1)), // axisW
+                        new Vec3d(0, 1, 0).rotateX((float) Math.toRadians(-rotation2)), // axisH
+                        INITAL_VISUAL_WIDTH, // width
+                        INITAL_VISUAL_HEIGHT // height
+                );
+                break;
+            case DOWN:
+                portal.setOrientationAndSize(
+                        new Vec3d(1, 0, 0).rotateY((float) Math.toRadians(rotation1)), // axisW
+                        new Vec3d(0, 1, 0).rotateX((float) Math.toRadians(rotation2)), // axisH
+                        INITAL_VISUAL_WIDTH, // width
+                        INITAL_VISUAL_HEIGHT // height
+                );
+                break;
+            default:
+                portal.setOrientationAndSize(
+                        new Vec3d(1, 0, 0).rotateY((float) Math.toRadians(rotation1)), // axisW
+                        new Vec3d(0, 1, 0), // axisH
+                        INITAL_VISUAL_WIDTH, // width
+                        INITAL_VISUAL_HEIGHT // height
+                );
+                break;
+        }
     }
 
     public void shrinkPortal() {
@@ -217,10 +213,9 @@ public class SealPortalHelper extends AbstractSealHelper {
     }
 
     public void playSpawnInSounds() {
-
+        world.playSound(null, pos, DevilrySounds.CHAOS_PORTAL, SoundCategory.BLOCKS,1F, 1F);
     }
 
-    boolean growPortal = false;
 
     public void growPortal() {
         growPortal = true;
